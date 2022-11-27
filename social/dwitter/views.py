e1baa30a6f02ae91a98488673120e63ff2c327d3
RaneenDwikat <raneendwikat16@gmail.com>
2022-11-27 21:59:42 +0200

    part 2

--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -1,21 +1,30 @@
-from django.shortcuts import render
-from .models import Profile
+from django.shortcuts import render, redirect
+from .models import Profile, Dweet
 from .forms import DweetForm
 
+
 def dashboard(request):
+    form = DweetForm(request.POST or None)
     if request.method == "POST":
-        form = DweetForm(request.POST)
         if form.is_valid():
             dweet = form.save(commit=False)
             dweet.user = request.user
             dweet.save()
-    form = DweetForm()
-    return render(request, "dwitter/dashboard.html", {"form": form})
+            return redirect("dwitter:dashboard")
+    followed_dweets = Dweet.objects.filter(
+        user__profile__in=request.user.profile.follows.all()
+    ).order_by("-created_at")
+    return render(
+        request,
+        "dwitter/dashboard.html",
+        {"form": form, "dweets": followed_dweets},
+    )
 
 
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
 
 def profile(request, pk):
     if not hasattr(request.user, 'profile'):
